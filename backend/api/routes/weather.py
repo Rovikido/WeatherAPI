d221c9166c5b227eb789b2c37ae9852b6b837ab2
Oleh Sinkevych <oleh.o.sinkevych@protonmail.com>
2022-10-14 11:04:31 +0300

    Add example how to invoke route function within internal module

--- backend/api/routes/weather.py
+++ backend/api/routes/weather.py
@@ -12,13 +12,13 @@ BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 def build_weather_query(city: str, imperial=False) -> str:
     """Builds the URL for an API request to OpenWeather's weather API.
 
-      Args:
-          city (str): Name of a city as collected by argparse
-          imperial (bool): Whether or not to use imperial units for temperature
+    Args:
+        city (str): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
 
-      Returns:
-          str: URL formatted for a call to OpenWeather's city name endpoint
-      """
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name endpoint
+    """
     units = "imperial" if imperial else "metric"
     request_data = {'q': city,
                     'appid': WEATHER_API_KEY,
@@ -30,8 +30,22 @@ def build_weather_query(city: str, imperial=False) -> str:
 
 @router.get("/weather/")
 def get_weather_info(city: str, imperial=False):
-    """To fill!"""
+    """Returns current weather info from OpenWeather's weather API.
+
+    Args:
+        city (str): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    Returns:
+        weather_info (dict~json): current weather info in specified city.
+    """
     url = build_weather_query(city=city, imperial=imperial)
     response = requests.get(url)
     weather_info = response.json()
     return weather_info
+
+
+@router.get("/")
+def index():
+    """Just an index method:)"""
+    return "Weather app"
